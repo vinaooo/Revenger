cbd34e65ddb1109040e9d866c8a831384dbebae0
Vinicius Pedrinho <vrpedrinho@gmail.com>
2025-09-18 18:21:23 -0300

    Update project configuration and resources

--- app/src/main/java/com/draco/ludere/retroview/RetroView.kt
+++ app/src/main/java/com/draco/ludere/retroview/RetroView.kt
@@ -27,7 +27,7 @@ class RetroView(private val context: Context, private val compositeDisposable: C
         coreFilePath = "libcore.so"
 
         /* Prepare the ROM bytes */
-        val romInputStream = context.resources.openRawResource(R.raw.rom)
+        val romInputStream = context.resources.openRawResource(R.raw.sonic_the_hedgehog)
         if (resources.getBoolean(R.bool.config_load_bytes)) {
             if (romBytes == null)
                 romBytes = romInputStream.use {it.readBytes() }
