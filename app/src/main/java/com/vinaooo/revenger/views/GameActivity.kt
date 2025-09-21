2b0cc08821a14c66b10d8645b96eb259a51cd0cc
Vinicius Pedrinho <vrpedrinho@gmail.com>
2025-09-21 13:05:28 -0300

    Atualiza configurações do GameActivity

--- app/src/main/java/com/vinaooo/revenger/views/GameActivity.kt
+++ app/src/main/java/com/vinaooo/revenger/views/GameActivity.kt
@@ -5,20 +5,30 @@ import android.hardware.input.InputManager
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.widget.FrameLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.vinaooo.revenger.databinding.ActivityGameBinding
+import com.vinaooo.revenger.R
 import com.vinaooo.revenger.viewmodels.GameActivityViewModel
 
+/**
+ * Main game activity for the emulator
+ */
 class GameActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityGameBinding
+    private lateinit var leftContainer: FrameLayout
+    private lateinit var rightContainer: FrameLayout
+    private lateinit var retroviewContainer: FrameLayout
     private val viewModel: GameActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityGameBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_game)
+        
+        // Initialize views
+        leftContainer = findViewById(R.id.left_container)
+        rightContainer = findViewById(R.id.right_container)
+        retroviewContainer = findViewById(R.id.retroview_container)
 
         /* Use immersive mode when we change the window insets */
         window.decorView.setOnApplyWindowInsetsListener { view, windowInsets ->
@@ -28,10 +38,10 @@ class GameActivity : AppCompatActivity() {
 
         registerInputListener()
         viewModel.setConfigOrientation(this)
-        viewModel.updateGamePadVisibility(this, binding.leftContainer, binding.rightContainer)
+        viewModel.updateGamePadVisibility(this, leftContainer, rightContainer)
         viewModel.prepareMenu(this)
-        viewModel.setupRetroView(this, binding.retroviewContainer)
-        viewModel.setupGamePads(binding.leftContainer, binding.rightContainer)
+        viewModel.setupRetroView(this, retroviewContainer)
+        viewModel.setupGamePads(leftContainer, rightContainer)
     }
 
     /**
@@ -41,13 +51,13 @@ class GameActivity : AppCompatActivity() {
         val inputManager = getSystemService(Service.INPUT_SERVICE) as InputManager
         inputManager.registerInputDeviceListener(object : InputManager.InputDeviceListener {
             override fun onInputDeviceAdded(deviceId: Int) {
-                viewModel.updateGamePadVisibility(this@GameActivity, binding.leftContainer, binding.rightContainer)
+                viewModel.updateGamePadVisibility(this@GameActivity, leftContainer, rightContainer)
             }
             override fun onInputDeviceRemoved(deviceId: Int) {
-                viewModel.updateGamePadVisibility(this@GameActivity, binding.leftContainer, binding.rightContainer)
+                viewModel.updateGamePadVisibility(this@GameActivity, leftContainer, rightContainer)
             }
             override fun onInputDeviceChanged(deviceId: Int) {
-                viewModel.updateGamePadVisibility(this@GameActivity, binding.leftContainer, binding.rightContainer)
+                viewModel.updateGamePadVisibility(this@GameActivity, leftContainer, rightContainer)
             }
         }, null)
 
